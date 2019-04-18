1a848a7cbda351db32df86a5c7231961258a6203
feilong <feilongphone@gmail.com>
2019-04-18 09:13:34 +0800

    fix test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:avis/main.dart';
+import 'package:avis/MyApp.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
