dd83a8ba44191ea4c05cef6e686829b69c2c7a4f
feilong <feilongphone@gmail.com>
2019-04-25 07:16:42 +0800

    Crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,33 @@
+import 'dart:async';
+
 import 'package:avis/MyApp.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_crashlytics/flutter_crashlytics.dart';
 
 import 'common/AppShareData.dart';
 
-//void init() {
-//  initAppParse();
-//}
+void main() async {
+  bool isInDebugMode = false;
 
-void main() {
-//  init();
-  runApp(AppShareData(child: MyApp()));
+  FlutterError.onError = (FlutterErrorDetails details) {
+    if (isInDebugMode) {
+      // In development mode simply print to console.
+      FlutterError.dumpErrorToConsole(details);
+    } else {
+      // In production mode report to the application zone to report to
+      // Crashlytics.
+      Zone.current.handleUncaughtError(details.exception, details.stack);
+    }
+  };
+
+  await FlutterCrashlytics().initialize();
+
+  runZoned<Future<Null>>(() async {
+    runApp(AppShareData(child: MyApp()));
+  }, onError: (error, stackTrace) async {
+    // Whenever an error occurs, call the `reportCrash` function. This will send
+    // Dart errors to our dev console or Crashlytics depending on the environment.
+    await FlutterCrashlytics()
+        .reportCrash(error, stackTrace, forceCrash: false);
+  });
 }
